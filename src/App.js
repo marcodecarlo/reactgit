98642a8b7fa7946d2983f1bd9d8a8a6a0ba095d5
marco decarlo <marco.decarlo@dechit.it>
2020-12-01 21:33:28 +0100

    eliminazione nextjs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import Main from './components/common/Main';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Main/>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Issue/IssueDetails/IssueDetails.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Moment from 'moment';
+
+export default class  IssuesDetails extends Component {
+
+    render() {
+        let dateDMY = Moment(this.props.issue?.created_at).format('DD-MM-YYYY');
+    return(
+        <div className="border border-gray-300 p-6 rounded-lg h-full content-between flex flex-wrap" key={this.props.issue.id}>
+            <div className={"w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 "+(this.props.issue.labels[0]?.name).replace(/ /g,"_")}>
+                {this.props.issue.number}
+            </div>
+            <h2 className="text-lg  font-medium title-font ml-2">
+                {this.props.issue.title}
+            </h2>
+            <p className="leading-relaxed text-base">
+                {this.props.issue.body}
+            </p>
+
+            <div className="text-center mt-2 leading-none flex justify-between w-full">
+              <span className=" mr-3 inline-flex items-center leading-none text-sm  py-1 "></span>
+              <span className=" inline-flex items-center leading-none text-sm">
+                 Creata da : {this.props.issue.user?.login} il {dateDMY}
+              </span>
+            </div>
+        </div>
+    );
+    }
+};
