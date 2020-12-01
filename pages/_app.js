b300a319f4a7a23da394f3a51f6ee0f74ac77faf
marco decarlo <marco.decarlo@dechit.it>
2020-12-01 21:46:38 +0100

    eliminazione next

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/styles.css';
-import '../components/Issue/IssueDetails/IssueDetails.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
