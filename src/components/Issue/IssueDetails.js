5093e13c2d21394470505a8fbfc2b4edf4f31882
marco decarlo <marco.decarlo@dechit.it>
2020-12-04 23:36:25 +0100

    layout card issue

--- src/components/Issue/IssueDetails.js
+++ src/components/Issue/IssueDetails.js
@@ -12,26 +12,28 @@ export default class IssuesDetails extends Component {
         className="border border-gray-300 p-6 rounded-lg h-full content-between flex flex-wrap"
         key={this.props.issue.id}
       >
+        <div class="flex space-x-4 w-full justify-between">
         <div
           className={
-            "w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 " +
+            " w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 " +
             (this.props.issue.labels[0]?.name).replace(/ /g, "_")
           }
         >
           {this.props.issue.number}
         </div>
-        <h2 className="text-lg  font-medium title-font ml-2">
+        <h2 className="flex-2 title-card ml-2">
           {this.props.issue.title}
         </h2>
-        <p className="leading-relaxed text-base">{this.props.issue.body}</p>
-
-        <div className="text-center mt-4 leading-none flex justify-between w-full">
-          <span className=" mr-3 inline-flex items-center leading-none text-sm  py-1 ">
+        <span className="flex-1 ml-auto text-right">
             <span className="count-icon">
               <FontAwesomeIcon icon={faComment} size="lg" />
               <span className="count">{this.props.issue.comments}</span>
             </span>
           </span>
+        </div>
+        <p className="text-anteprima">{this.props.issue.body}</p>
+
+        <div className="text-center mt-4 leading-none flex justify-between w-full">
           <span className=" inline-flex items-center leading-none text-sm">
             Creata da : {this.props.issue.user?.login} il {dateDMY}
           </span>
--- src/lib/api/issues/IssuesService.js
+++ src/lib/api/issues/IssuesService.js
@@ -14,3 +14,4 @@ class IssuesService {
 }
 
 export default new IssuesService();
+       
